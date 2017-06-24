51f434824af24e1ce04cc5f5150df9fec443851f
Tyler Cvetan <tcvetan@connecture.com>
2017-06-24 13:13:38 -0500

    Starting with day support

--- demo/demo.js
+++ demo/demo.js
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom';
 import Planner from '../src/Planner.js';
 
 ReactDOM.render(
-  <div><Planner /></div>
+  <div><Planner days={[{ label: 'Sunday' }, { label: 'Monday' }, { label: 'Tuesday' }]} /></div>
 , document.getElementById('app'));
--- src/Planner.js
+++ src/Planner.js
@@ -1,33 +1,42 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import Dragula from 'react-dragula';
 
 export default class Planner extends PureComponent {
+  static propTypes = {
+    days: PropTypes.arrayOf(
+      PropTypes.shape({
+        label: PropTypes.string
+      })
+    ).isRequired
+  }
+
+  constructor(props) {
+    super(props);
+    this.dayContainers = [];
+  }
 
   componentDidMount() {
-    Dragula([this.container1, this.container2], {});
+    Dragula(this.dayContainers, {});
   }
 
   render() {
+    const { days } = this.props;
     return (
       <div>
-        <div className="container" ref={container => { this.container1 = container; }}>
-          <div>Swap me around</div>
-          <div>Swap her around</div>
-          <div>Swap him around</div>
-          <div>Swap them around</div>
-          <div>Swap us around</div>
-          <div>Swap things around</div>
-          <div>Swap everything around</div>
-        </div>
-        <div className="container" ref={container => { this.container2 = container; }}>
-          <div>Swap me around</div>
-          <div>Swap her around</div>
-          <div>Swap him around</div>
-          <div>Swap them around</div>
-          <div>Swap us around</div>
-          <div>Swap things around</div>
-          <div>Swap everything around</div>
-        </div>
+        {days.map(day => {
+          return (
+            <div className="container" ref={container => { this.dayContainers.push(container); }}>
+              <div>Swap me around</div>
+              <div>Swap her around</div>
+              <div>Swap him around</div>
+              <div>Swap them around</div>
+              <div>Swap us around</div>
+              <div>Swap things around</div>
+              <div>Swap everything around</div>
+            </div>
+          );
+        })}
       </div>
     );
   }
